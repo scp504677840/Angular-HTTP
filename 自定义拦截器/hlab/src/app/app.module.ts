ed185a385dfb8f65ae0356671f901737ca91ea2b
y1笑而过 <15335747148@163.com>
2018-07-22 22:35:16 +0800

    自定义拦截器
    自定义拦截器

---
+++ 自定义拦截器/hlab/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {UsersComponent} from './users/users.component';
+import {UserSearchComponent} from './users/user-search/user-search.component';
+import {httpInterceptorProviders} from './http-interceptors';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    UserSearchComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [httpInterceptorProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
